28ab1580139c8f4eb6245dfc345c57466df330a2
MagicaQuartet <dev.dskim@gmail.com>
2020-05-09 13:58:06 +0000

    Add checkbox to todo-item
    할 일의 완료 여부를 입력하는 체크박스를 추가하였습니다.

--- frontend/src/components/todo/TodoItem.vue
+++ frontend/src/components/todo/TodoItem.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="todo-item">
-    <div class="item-content">{{ content }}</div>
-    <div class="item-datetime">{{ datetimeString }}</div>
+  <div class="todo-item" :class="{ completed: completed }">
+    <label class="item-checkbox">
+      <input type="checkbox" v-model="completed">
+      <component :is="checkboxIcon" font-scale="2"></component>
+    </label>
+    <span class="item-content">{{ content }}</span>
+    <span class="item-datetime">{{ datetimeString }}</span>
   </div>
 </template>
 
 <script>
+  import { BIconCircle, BIconCheckCircle } from 'bootstrap-vue'
+  
   export default {
     name: "TodoItem",
+    components: {
+      BIconCircle,
+      BIconCheckCircle
+    },
     props: ['content', 'datetime'],
+    data: function() {
+      return {
+        completed: false
+      } 
+    },
     computed: {
       datetimeString: function() {
         return `
@@ -18,6 +33,9 @@
           ${this.datetime.getHours()}시 \
           ${this.datetime.getMinutes()}분
         `;
+      },
+      checkboxIcon: function() {
+        return this.completed ? 'b-icon-check-circle' : 'b-icon-circle';
       }
     }
   }
@@ -26,13 +44,31 @@
 <style>
   .todo-item {
     display: flex;
-    justify-content: space-between;
     align-items: center;
     padding: 0.8rem;
     margin: 10px 0;
     height: 3.6rem;
     border-radius: 10px;
     background-color: #eeeeee;
+  }
+  
+  .todo-item > * {
+    padding: 0 0.25rem;
+  }
+  
+  .todo-item.completed .item-content {
+    text-decoration: line-through;
+  }
+  
+  .item-checkbox {
+    margin: 0;
+  }
+  
+  .item-checkbox input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
   }
   
   .item-content {
