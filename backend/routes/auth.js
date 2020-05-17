c491e922d3aff21d084eea2f99ee2d2285dcaf27
MagicaQuartet <dev.dskim@gmail.com>
2020-05-17 02:17:14 +0000

    Implement sign-up process
    간단한 회원가입 기능을 구현하였습니다.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,17 +3,27 @@ var router = express.Router();
 
 var User = require('../models/user');
 
-router.post('/', function(req, res) {
-  res.send('auth!');
-})
-
 router.post('/login', function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
   
   User.find({ username, password })
-  .then(user => res.send(user.length ? 'OK' : 'FAILED'))
-  .catch(err => console.log(err))
+  .then((user) => res.send(user.length ? 'OK' : 'FAILED'))
+  .catch((err) => console.log(err))
+});
+
+router.post('/register', function(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    username,
+    password
+  });
+  
+  user.save()
+  .then(() => res.send('Saved successfully:', user.username))
+  .catch((err) => console.log(err));
 });
 
 module.exports = router;
