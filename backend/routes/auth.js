5e51df2472a02adfb2d4ba55d31739f6110a3f64
MagicaQuartet <dev.dskim@gmail.com>
2020-06-03 15:57:56 +0900

    Use bcrypt to hash password
    패스워드를 해쉬로 바꾸어 저장하기 위해 bcrypt 패키지를 사용합니다.
    로그인/회원가입 시 bcrypt를 사용하여 비밀번호 생성 및 대조를 합니다.
    
    (본 커밋 내용부터는 VSCode 플러그인의 영향으로 코드 포맷팅 수정으로 인한
     변경 사항도 포함될 수 있습니다.)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,36 +1,48 @@
-var express = require('express');
+var bcrypt = require("bcrypt-nodejs");
+var express = require("express");
 var router = express.Router();
 
-var User = require('../models/user');
+var User = require("../models/user");
 
-router.post('/login', function(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  
-  User.find({ username, password })
-  .then((user) => res.send({ success: user.length > 0 }))
-  .catch((err) => console.log(err))
-});
-
-router.post('/register', function(req, res, next) {
+router.post("/login", function (req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
 
   User.find({ username })
-  .then(function(result) {
-    if (result.length) {
-      return false;
-    }
-    
-    const user = new User({
-      username,
-      password
-    });
-      
-    return user.save().then(() => true);
-  })
-  .then((success) => res.status(200).send({ success }))
-  .catch((err) => console.log(err));
+    .then((user) => {
+      if (user.length === 0) {
+        res.send({ success: false });
+      } else {
+        bcrypt.compare(password, user[0].password, function (err, _res) {
+          res.send({ success: _res });
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/register", function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.find({ username })
+    .then(function (result) {
+      if (result.length > 0) {
+        return false;
+      }
+
+      const user = new User({
+        username,
+      });
+
+      bcrypt.hash(password, null, null, function (err, hash) {
+        user.password = hash;
+      });
+
+      return user.save();
+    })
+    .then(() => res.status(200).send({ success: true }))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
